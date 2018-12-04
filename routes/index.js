d35fb6c93c6c903e6a5768ae9fe1d75c4ad79cf5
Brett Hartman <brett.hartman@mac.com>
2018-12-04 11:48:03 -0800

    Design landing page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-	res.render('index', { title: 'Code4Share' });
+	res.render('index', { title: 'Home' });
+});
+
+router.get('/about', (req, res) => {
+	res.render('about', { title: 'About Us' });
+});
+
+router.get('/contact', (req, res) => {
+	res.render('contact', { title: 'Contact Us' });
 });
 
 module.exports = router;
