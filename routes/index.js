0f387f4db838277ce01ae3d551168e0d65ddb9a0
Brett Hartman <brett.hartman@mac.com>
2019-03-23 15:34:45 -0700

    Implement contact route and form

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator/check');
 
 const router = express.Router();
 
@@ -15,8 +16,37 @@ router
 	.get((req, res) => {
 		res.render('contact', { title: 'Contact Us' });
 	})
-	.post((req, res) => {
-		res.render('thanks', { title: 'Message Received' });
-	});
+	.post(
+		[
+			check('name', 'You must supply your name.')
+				.not()
+				.isEmpty()
+				.trim()
+				.escape(),
+			check('email', 'The email address you entered is invalid.')
+				.isEmail()
+				.normalizeEmail(),
+			check('message', 'Please include a message.')
+				.not()
+				.isEmpty()
+				.trim()
+				.escape()
+		],
+		(req, res) => {
+			const errors = validationResult(req);
+
+			if (!errors.isEmpty()) {
+				return res.render('contact', {
+					title: 'Contact Us',
+					name: req.body.name,
+					email: req.body.email,
+					message: req.body.message,
+					errors
+				});
+			}
+
+			res.render('thanks', { title: 'Message Received' });
+		}
+	);
 
 module.exports = router;
